f2a5872cf6d836bc185ba3ce466434933dd7f305
Keymoney001 <kimanimichaek40@gmail.com>
2025-08-12 11:41:16 +0000

    Restored to 'c7a5299e25dbbe61d64156d0e19627f489e045cf'
    Replit-Restored-To: c7a5299e25dbbe61d64156d0e19627f489e045cf

--- src/app/app-content.jsx
+++ src/app/app-content.jsx
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react-lite';
 import { ToastContainer } from 'react-toastify';
 import useLiveChat from '@/components/chat/useLiveChat';
 import ChunkLoader from '@/components/loader/chunk-loader';
+import MatrixLoading from '@/components/matrix-loading';
 import { getUrlBase } from '@/components/shared';
 import TncStatusUpdateModal from '@/components/tnc-status-update-modal';
 import TransactionDetailsModal from '@/components/transaction-details';
@@ -162,7 +163,7 @@ const AppContent = observer(() => {
             initHotjar(client);
         }
     }, []);
-
+    
      React.useEffect(() => {
         const timeout = setTimeout(() => {
             setForceShowApp(true);
@@ -184,7 +185,7 @@ const AppContent = observer(() => {
     if (common?.error) return null;
 
     return is_loading && !forceShowApp ? (
-        <></>
+        <MatrixLoading message={localize('Initializing your account...')} show={true} />
     ) : (
         <>
             <ThemeProvider theme={is_dark_mode_on ? 'dark' : 'light'}>
