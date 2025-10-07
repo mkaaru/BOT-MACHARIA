e93eedf75b9bd425e070573fc1bd7cadcd24da9f
smartrader1989 <42150220-smartrader1989@users.noreply.replit.com>
2025-10-07 10:13:09 +0000

    Restored to 'b88388da15336ed619d2c9f3f9852ca9b66c74de'
    Replit-Restored-To: b88388da15336ed619d2c9f3f9852ca9b66c74de

--- src/external/bot-skeleton/services/tradeEngine/Interface/BotInterface.js
+++ src/external/bot-skeleton/services/tradeEngine/Interface/BotInterface.js
@@ -1,6 +1,5 @@
 import { observer as globalObserver } from '../../../utils/observer';
 import { createDetails } from '../utils/helpers';
-import botStrategyManager from '../../bot-strategy-manager';
 
 const getBotInterface = tradeEngine => {
     const getDetail = i => createDetails(tradeEngine.data.contract)[i];
@@ -9,35 +8,16 @@ const getBotInterface = tradeEngine => {
         init: (...args) => tradeEngine.init(...args),
         start: (...args) => tradeEngine.start(...args),
         stop: (...args) => tradeEngine.stop(...args),
-        purchase: (contract_type, tradeEachTick = false) => {
-            // Apply contract type alternation strategy
-            const actual_contract_type = botStrategyManager.getContractType(contract_type);
-            return tradeEngine.purchase(actual_contract_type, tradeEachTick);
-        },
+        purchase: (contract_type, tradeEachTick = false) => tradeEngine.purchase(contract_type, tradeEachTick),
         getAskPrice: contract_type => Number(getProposal(contract_type, tradeEngine).ask_price),
         getPayout: contract_type => Number(getProposal(contract_type, tradeEngine).payout),
         getPurchaseReference: () => tradeEngine.getPurchaseReference(),
         isSellAvailable: () => tradeEngine.isSellAtMarketAvailable(),
         sellAtMarket: () => tradeEngine.sellAtMarket(),
         getSellPrice: () => getSellPrice(tradeEngine),
-        isResult: result => {
-            const is_result = getDetail(10) === result;
-            
-            // Update strategy based on result (only if we have a current contract type)
-            if (is_result && tradeEngine.contractId) {
-                const contract_type = botStrategyManager.current_contract_type;
-                if (contract_type) {
-                    botStrategyManager.updateResult(contract_type, result);
-                }
-            }
-            
-            return is_result;
-        },
+        isResult: result => getDetail(10) === result,
         isTradeAgain: result => globalObserver.emit('bot.trade_again', result),
         readDetails: i => getDetail(i - 1),
-        getBotStrategyState: () => botStrategyManager.getState(),
-        resetBotStrategy: () => botStrategyManager.reset(),
-        setBotStrategyEnabled: (enabled) => botStrategyManager.setEnabled(enabled),
     };
 };
 
